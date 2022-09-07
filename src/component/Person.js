c181161539f33ea53d0318c2d55f46cb3cf407ed
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-09-07 13:06:27 +0300

    fix the error

--- src/component/Person.js
+++ src/component/Person.js
@@ -1,82 +1,83 @@
-import axios from "axios";
-import React, {useState} from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import axios from 'axios';
+import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import '../index.css';
 
 export default function Person() {
-    const [name, setName] = useState('Enter your name');
-    const [age, setAge] = useState('Enter your age');
-    const [gender, setGender] = useState('Enter your gender');
-    const [newAge, setNewAge] = useState('Your Age + 5 Years');
+  const [name, setName] = useState('your name');
+  const [age, setAge] = useState('your age');
+  const [gender, setGender] = useState('your gender');
+  const [newAge, setNewAge] = useState('Your Age + 5 Years');
 
-    function handleSubmmit(e) {
-        e.preventDefault();
-        // setName(e.target.name.value);
-        // setAge(e.target.age.value);
-        // setGender(e.target.gender.value);
-        // setNewAge(parseInt(age) + 5);
-        getNewAge();
-    }
+  function handleSubmmit(e) {
+    e.preventDefault();
+    // setName(e.target.name.value);
+    // setAge(e.target.age.value);
+    // setGender(e.target.gender.value);
+    // setNewAge(parseInt(age) + 5);
+    getNewAge();
+  }
 
-    function handleNameChange(e) {
-        setName(e.target.value);
-    }
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
 
-    function handleAgeChange(e) {
-        setAge(e.target.value);
-    }
+  function handleAgeChange(e) {
+    setAge(e.target.value);
+  }
 
-    function handleGenderChange(e) {
-        setGender(e.target.value);
-    }
+  function handleGenderChange(e) {
+    setGender(e.target.value);
+  }
 
-    async function getNewAge() {
-        await axios.post('https://express-server401.herokuapp.com/person')
-        .then(res => {
-            setNewAge(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
+  async function getNewAge() {
+    await axios.post('https://express-server401.herokuapp.com/person')
+      .then(res => {
+        setNewAge(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
 
-    return (
-        <div className="person-perant">
-            <Form onSubmit={handleSubmmit} className="person-child">
+  return (
+    <div className="person-perant">
 
-            <Form.Group>
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Name" id="Name"  data-testid='input-name' onChange={handleNameChange}/>
-            </Form.Group>
+      <Form onSubmit={handleSubmmit} className="person-child">
 
-            <Form.Group>
-              <Form.Label>Age</Form.Label>
-              <Form.Control type="text" placeholder="Enter Age" id="Age"   data-testid='input-age' onChange={handleAgeChange}/>
-            </Form.Group>
-            <br></br>
+        <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter Name" id="Name" data-testid='input-name' onChange={handleNameChange}/>
+        </Form.Group>
 
-            <Form.Select aria-label="Default select example" id="Gender"  data-testid='input-gender' onChange={handleGenderChange}> 
-              <option>select your gender ...</option>
-              <option value="Male">Male</option>
-              <option value="Female">Female</option>
-            </Form.Select>
-            <br></br>
+        <Form.Group>
+          <Form.Label>Age</Form.Label>
+          <Form.Control type="text" placeholder="Enter Age" id="Age" data-testid='input-age' onChange={handleAgeChange}/>
+        </Form.Group>
+        <br></br>
 
-            <Button variant="outline-dark" type="submit" data-testid="submit">
-              Submit
-            </Button>
-        
-            <Form.Group>
-              <Form.Label>-------------------------------------------------------------------------</Form.Label> <br></br>
-              <Form.Label data-testid='Name'>Name: {name}</Form.Label> <br></br>
-              <Form.Label data-testid='Age'>Age: {age}</Form.Label> <br></br>
-              <Form.Label  data-testid='Gender'>Gender: {gender}</Form.Label> <br></br>
-              {/* <Form.Label  id="new-age" data-testid='NewAge'>New Age: {newAge}</Form.Label> */}
-            </Form.Group>
+        <Form.Select aria-label="Default select example" id="Gender" data-testid='input-gender' onChange={handleGenderChange}>
+          <option>select your gender ...</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </Form.Select>
+        <br></br>
 
-            </Form>  
-        </div>  
-    );         
+        <Button variant="outline-dark" type="submit" data-testid="submit">
+          Submit
+        </Button>
+
+        <Form.Group>
+          <Form.Label>-------------------------------------------------------------------------</Form.Label> <br></br>
+          <Form.Label data-testid='Name'>Name: {name}</Form.Label> <br></br>
+          <Form.Label data-testid='Age'>Age: {age}</Form.Label> <br></br>
+          <Form.Label data-testid='Gender'>Gender: {gender}</Form.Label> <br></br>
+          <Form.Label id="new-age" data-testid='NewAge'>New Age: {newAge}</Form.Label>
+        </Form.Group>
+
+      </Form>
+    </div>
+  );
 }
