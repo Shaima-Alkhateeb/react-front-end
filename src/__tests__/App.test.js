e50529e60d0b361c99970d0c76a6ae26c1ba5ed8
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-09-09 18:45:46 +0300

    done

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -17,11 +17,37 @@ test('Can change the data', async () => {
     const inputNmae = screen.getByTestId('input-name');
     const inputAge = screen.getByTestId('input-age');
     const inputGender = screen.getByTestId('input-gender');
+    // const inputNewAge = screen.getByTestId('input-gender');
 
     fireEvent.change(inputNmae, { target: { value: 'shaima' } });
     fireEvent.change(inputAge, { target: { value: '55' } });
     fireEvent.change(inputGender, { target: { value: 'female' } });
+    // fireEvent.change(inputGender, { target: { value: 'female' } });
 
     // expect().toHaveTextContent()
 
   })
+
+
+  // add 5 with the age
+  // test('age + 5 test', async () => {
+  //   render(<Person />);
+  //   const ageInput = screen.getByTestId("input-name");
+  //   const submit = screen.getByTestId('submit');
+
+  //   fireEvent.change(ageInput, { target: { value: '55' } });
+  //   fireEvent.click(submit);
+
+  //   const  waitFor = () => screen.getByTestId('new-age')
+  //   const newAge = await waitFor();
+  //   setTimeout(() => {
+  //     expect(newAge).toHaveTextContent('New Age: 60');
+  //   }, 2000);
+
+
+    // expect(newAge).toHaveTextContent('60');
+    // expect(newAge).toBeInTheDocument();
+
+
+
+  // });
